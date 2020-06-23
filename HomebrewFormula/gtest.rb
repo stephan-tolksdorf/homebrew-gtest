bda80d32e6559a278eb4077730b0ba6c39afde36
Stephan Tolksdorf <st@quanttec.com>
2020-06-23 14:58:51 +0200

    Initial commit

---
+++ HomebrewFormula/gtest.rb
@@ -0,0 +1,19 @@
+class Gtest < Formula
+  desc "Google Test"
+  homepage "https://github.com/google/googletest"
+  head "git://github.com/google/googletest.git", :using => :git
+
+  stable do
+    url "https://github.com/google/googletest/archive/release-1.10.0.tar.gz"
+    sha256 "9dc9157a9a1551ec7a7e43daea9a694a0bb5fb8bec81235d8a1e6ef64c716dcb"
+  end
+
+  depends_on "cmake" => :build
+
+  def install
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make", "install"
+    end
+  end
+end
